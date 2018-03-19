5f15bbeb1f1b77177a52fae2490b4aef3a1e6a5e
Kumaran Ravichandran <mailkumaranravi@gmail.com>
2018-03-19 00:33:28 -0400

    update profile modal changes

--- src/app/global/navigator/navigator.component.ts
+++ src/app/global/navigator/navigator.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NAVITEMS} from '../../../assets/data/nav-items';
 import {User} from '../../common/types/User';
 import {UserStoreService} from '../../stores/user-store.service';
+import {FormControl, FormControlName, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-navigator',
@@ -12,12 +13,26 @@ import {UserStoreService} from '../../stores/user-store.service';
 export class NavigatorComponent implements OnInit {
   navItems = NAVITEMS;
   user: User;
+  updateUserForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
 
   constructor(private _userStore: UserStoreService) {
   }
 
   ngOnInit() {
     this._userStore.getUser().subscribe(user => this.user = user);
+  }
+
+  updateUser(updateUserForm) {
+    console.log(updateUserForm);
+    // TODO call api to update user here...
+    // TODO check why only updated fields are being populated here...
   }
 
 }
